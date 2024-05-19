1678939004e404a654286fcb7c0420cbd52c39c0
Priyanshi <priyanshibr@gmail.com>
2024-05-19 21:18:52 +0530

    style changes

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import Container from "./Container"
 function App() {
 
   return (    
-    <div className="flex justify-center items-center w-full h-full">
+    <div className="flex justify-center items-center align-middle w-full h-full">
       <Container />
     </div>
   )
